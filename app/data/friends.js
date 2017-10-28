2d0101ac8b2d4ce23884ec9e6f41a01febe0a466
Loren <lorenrose@icloud.com>
2017-10-27 23:51:47 -0500

    files

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendList = [
+  {
+    name: 'Loren Rose',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
